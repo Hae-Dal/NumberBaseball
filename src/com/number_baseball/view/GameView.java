dcf123e08c188d4ad034e86849af0aab01e8d813
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-09-13 19:23:06 +0900

    변경 : displayInputResult 메서드의 인수 자료형 변경 구현 : 입력받을 경우 안내메시지 출력 메서드, 정답메시지 표시 메서드

--- src/com/number_baseball/view/GameView.java
+++ src/com/number_baseball/view/GameView.java
@@ -9,11 +9,19 @@ public class GameView {
                 """);
     }
 
-    public void displayInputResult(List<Integer> status) {
-        System.out.println(status.get(0) + " 스트라이크, " + status.get(1) + " 볼" + status.get(2) + " 아웃");
+    public void displayInputResult(int[] status) {
+        System.out.println(status[0] + " 스트라이크, " + status[1] + " 볼" + status[2] + " 아웃");
     }
 
     public void displayException(Exception e) {
         System.out.println(e.getMessage());
+    }
+
+    public void displayInputMessage(String message) {
+        System.out.print(message);
+    }
+
+    public void displayEndMessage() {
+        System.out.println("정답입니다. 축하합니다!");
     }
 }
