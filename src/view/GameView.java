fee5877ef561a3459d167da18dd3ce9964963876
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-09-13 17:53:17 +0900

    구현 : GameView 구현

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -1,4 +1,19 @@
 package view;
 
+import java.util.List;
+
 public class GameView {
+    public void displayGame() {
+        System.out.println("""
+                < 게임 시작 >
+                """);
+    }
+
+    public void displayInputResult(List<Integer> status) {
+        System.out.println(status.get(0) + " 스트라이크, " + status.get(1) + " 볼" + status.get(2) + " 아웃");
+    }
+
+    public void displayException(Exception e) {
+        System.out.println(e.getMessage());
+    }
 }
